f13cce4f38404d7ec3a685734a32086ac6e7e58c
JesusDiazDiaz <jesusd.diazdiaz@gmail.com>
2016-12-05 14:39:18 -0500

    se añadio cancion la cucaracha :D
    :V

--- Relog_Alarma.ino
+++ Relog_Alarma.ino
@@ -15,6 +15,31 @@ int minuto_alarma = 1;
 int segundo_alarma = 1;
 int digPin = 10;
 
+//cancion
+int ledPin = 13;
+int speakerOut = 10;
+
+/* 2 octavas :: semitones. 0 = do, 2 = re, etc. */
+/* MIDI notes from 48 to 71. Indices here are from 0 to 23. */
+
+int timeUpDown[] = {3822, 3606, 3404, 3214, 3032, 2862,
+2702, 2550, 2406, 2272, 2144, 2024,
+1911, 1803, 1702, 1607, 1516, 1431,
+1351, 1275, 1203, 1136, 1072, 1012};
+/* our song. Each number is a (MIDI note - 48) on a beat. */
+
+//byte song[] = {12,12,12,14, 16,16,14,14, 12,16,14,14, 12,12,12,12,
+//14,14,14,14, 9,9,9,9, 14,12,11,9, 7,7,7,7};
+byte song[] = {7,7,7,12,12,12,16,16,7,7,7,12,12,12,16,16,16,16,16,16,16,12,12,11,11,9,9,7,7,7,7,7};
+// do do do re mi re do mi re re do...
+
+byte beat = 0;
+int MAXCOUNT = 32;
+float TEMPO_SECONDS = 0.2;
+byte statePin = LOW;
+byte period = 0;
+int i, timeUp;
+
 
 //Varables para leer las teclas y el ADC
 int lcd_key     = 0;
@@ -48,7 +73,9 @@ int read_LCD_buttons()
  
 
 void setup() {
-  pinMode(13, OUTPUT);
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(speakerOut, OUTPUT);
   lcd.begin(16, 2);                 //LCD de 16 columnas y 2 filas
   lcd.clear();                      //Borrar la pantalla
   lcd.setCursor(0, 0);              //Llevar el cursor al inicio
@@ -66,10 +93,12 @@ void setup() {
   lcd.print("Ingrese hora");
   delay(100);
   while(lcd_key != btnSELECT){
+    //capturar boton presionado
     lcd_key = read_LCD_buttons();
     delay(50);
     lcd.clear();
     lcd.setCursor(0,0);
+    //verificar boton arriba
     if(lcd_key == btnARRIBA){
       hora_alarma++;
       if(hora_alarma == 25){
@@ -77,7 +106,7 @@ void setup() {
       }
       delay(200);
     }
-    
+    //verificar boton abajo
     if(lcd_key == btnABAJO){
       hora_alarma--;
       if(hora_alarma == 0){
@@ -136,7 +165,7 @@ void loop() {
 void Alarma(){
   lcd.clear();
   lcd.print("Alarma sonando!!");
-  sonar();
+  cancion();//lamada de la fucion
   delay(10000);
 }
 
@@ -145,6 +174,29 @@ void sonar(){
   analogWrite(digPin, 127);
   delay(10000);
   analogWrite(digPin, 0);
+}
+
+//funcion cancion la cucaracha :D
+void cancion(){
+  digitalWrite(speakerOut, LOW);
+  for (beat = 0; beat < MAXCOUNT; beat++) {
+    statePin = !statePin;
+    digitalWrite(ledPin, statePin);
+    
+    timeUp = timeUpDown[song[beat]];
+    
+    period = ((1000000 / timeUp) / 2) * TEMPO_SECONDS;
+    for (i = 0; i < period; i++) {
+    digitalWrite(speakerOut, HIGH);
+    delayMicroseconds(timeUp);
+    digitalWrite(speakerOut, LOW);
+    delayMicroseconds(timeUp);
+  }
+  /* Uncomment this if you want notes to be discrete */
+  /* delay(50); */
+  }
+  digitalWrite(speakerOut, LOW);
+  delay(1000);
 }
 
 //funcion para mostrar la hora
